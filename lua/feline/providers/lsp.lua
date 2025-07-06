e34601df85dadb2ebe9528ffd83f68abdfe64fd2
Adam Price <adpce@protonmail.com>
2025-07-06 01:14:04 +0100

    fix: replace deprecated method

--- lua/feline/providers/lsp.lua
+++ lua/feline/providers/lsp.lua
@@ -4,48 +4,48 @@ local lsp = vim.lsp
 local diagnostic = vim.diagnostic
 
 function M.is_lsp_attached()
-    return next(lsp.buf_get_clients(0)) ~= nil
+	return next(lsp.get_clients()) ~= nil
 end
 
 function M.get_diagnostics_count(severity)
-    return vim.tbl_count(diagnostic.get(0, severity and { severity = severity }))
+	return vim.tbl_count(diagnostic.get(0, severity and { severity = severity }))
 end
 
 function M.diagnostics_exist(severity)
-    return M.get_diagnostics_count(severity) > 0
+	return M.get_diagnostics_count(severity) > 0
 end
 
 function M.lsp_client_names()
-    local clients = {}
+	local clients = {}
 
-    for _, client in pairs(lsp.buf_get_clients(0)) do
-        clients[#clients + 1] = client.name
-    end
+	for _, client in pairs(lsp.get_clients()) do
+		clients[#clients + 1] = client.name
+	end
 
-    return table.concat(clients, ' '), ' '
+	return table.concat(clients, ' '), ' '
 end
 
 -- Common function used by the diagnostics providers
 local function diagnostics(severity)
-    local count = M.get_diagnostics_count(severity)
+	local count = M.get_diagnostics_count(severity)
 
-    return count ~= 0 and tostring(count) or ''
+	return count ~= 0 and tostring(count) or ''
 end
 
 function M.diagnostic_errors()
-    return diagnostics(diagnostic.severity.ERROR), '  '
+	return diagnostics(diagnostic.severity.ERROR), '  '
 end
 
 function M.diagnostic_warnings()
-    return diagnostics(diagnostic.severity.WARN), '  '
+	return diagnostics(diagnostic.severity.WARN), '  '
 end
 
 function M.diagnostic_info()
-    return diagnostics(diagnostic.severity.INFO), '  '
+	return diagnostics(diagnostic.severity.INFO), '  '
 end
 
 function M.diagnostic_hints()
-    return diagnostics(diagnostic.severity.HINT), '  '
+	return diagnostics(diagnostic.severity.HINT), '  '
 end
 
 return M
